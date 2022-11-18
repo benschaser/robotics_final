982a7981d40c390a19044b01f97f0b6caa315013
Ben Schaser <benschaser@gmail.com>
2022-11-18 15:55:37 -0600

    gyroscope testing

--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,60 @@
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_StepperMotor *leftMotor = AFMS.getStepper(200, 2);
-Adafruit_StepperMotor *rightMotor = AFMS.getStepper(200, 1);
+Adafruit_MPU6050 mpu;
 
 void setup() {
-//    startPing();
-    AFMS.begin();
-    leftMotor->setSpeed(300);
-//    rightMotor->setSpeed(150);
-    
-    
+  Serial.begin(9600);
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+  }
+  else {
+    Serial.println("MPU6050 Found!");
+  }
+  
+
+  // set accelerometer range to +-8G
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  // set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  // set filter bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
 }
+
 void loop() {
-  leftMotor->step(1, FORWARD, DOUBLE);
-//  rightMotor->step(100, FORWARD, DOUBLE);
-}
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  Serial.flush();
 
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
 
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
 
-void startPing() {
-  tone(3, 500, 600);
-  delay(600);
-  tone(3, 750, 100);
-  delay(100);
-  tone(3, 1000, 50);
-  delay(50);
-  tone(3, 1500, 50);
-  delay(50);
-  tone(3, 2000, 50);
-  delay(50);
-  tone(3, 1500, 50);
-  delay(50);
-  tone(3, 1000, 100);
-  delay(100);
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(2000);
 }
