0255be32fbba0dfee36c6ed0aa72134c206913ad
Ben Schaser <benschaser@gmail.com>
2022-11-28 23:08:29 -0600

    PID testing

---
+++ test/PID-test/PID-test.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include "math.h"
+
+Adafruit_MPU6050 mpu;
+
+int accX, accZ;
+double accAngle;
+int gyroY, gyroRate;
+double gyroAngle=0;
+unsigned long currTime, prevTime=0, loopTime;
+double currentAngle = 0;
+double prevAngle = 0;
+double tau = 0.75;
+double alpha = 0;
+double dt = 0.01;
+
+void setup() {
+  Serial.begin(9600);
+  mpu.begin();
+
+  // set accelerometer range to +-8G
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  // set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  // set filter bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+}
+
+void loop() {
+  prevAngle = currentAngle;
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  
+  currTime = millis();
+  loopTime = currTime - prevTime;
+  prevTime = currTime;
+  
+  accZ = a.acceleration.z;
+  accX = a.acceleration.x;
+   
+  accAngle = atan2(accX, accZ)*RAD_TO_DEG;
+
+  gyroY = g.gyro.y;
+  gyroRate = map(gyroY, -32768, 32767, -250, 250);
+  gyroAngle = gyroAngle + (double)gyroRate*loopTime/1000;
+
+  // complementary filter (high and low pass filter)
+  alpha = tau / (tau + dt);
+  currentAngle = alpha*(prevAngle + gyroAngle) + (1 - alpha)*accAngle;
+  
+  Serial.println(currentAngle);
+}
