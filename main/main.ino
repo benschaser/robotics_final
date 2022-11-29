5faf6b212693759e9ae2b8c8265a00d81fb38ae3
Ben Schaser <benschaser@gmail.com>
2022-11-29 16:22:11 -0600

    adjusted pid constants

--- main/main.ino
+++ main/main.ino
@@ -9,9 +9,10 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *leftMotor = AFMS.getStepper(200, 2);
 Adafruit_StepperMotor *rightMotor = AFMS.getStepper(200, 1);
 
-int16_t accX, accZ;
+// Angle of Inclination
+int accX, accZ;
 double accAngle;
-int16_t gyroY, gyroRate;
+int gyroY, gyroRate;
 double gyroAngle=0;
 unsigned long currTime, prevTime=0, loopTime;
 double currentAngle = 0;
@@ -19,6 +20,16 @@ double prevAngle = 0;
 double tau = 0.1;
 double dt = 0.01;
 double alpha = tau / (tau + dt);
+
+// PID Constants
+double Kp = 3.44;
+double Ki = 0.048;
+double Kd = 1.92;
+
+double targetAngle = 0;
+double difference = 0;
+double difSum = 0;
+double motorSpeed = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -32,21 +43,24 @@ void setup() {
 
   // set filter bandwidth to 21 Hz
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  AFMS.begin();
 }
 void loop() {
   currTime = millis();
-  loopTime = (currTime - prevTime) / 1000;
+//  Serial.println(currTime - prevTime);
+  loopTime = (currTime - prevTime);
   prevTime = currTime;
   
   getCurrentAngle(loopTime);
-  Serial.println(currentAngle);
+  calcSpeed();
+  Serial.println(motorSpeed);
+  prevAngle = currentAngle;
 }
 
-double getCurrentAngle(double elapsedTime) {
+void getCurrentAngle(unsigned long elapsedTime) {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  
-  
   
   accZ = a.acceleration.z;
   accX = a.acceleration.x;
@@ -55,10 +69,14 @@ double getCurrentAngle(double elapsedTime) {
 
   gyroY = g.gyro.y;
   gyroRate = map(gyroY, -32768, 32767, -250, 250);
-  gyroAngle = gyroAngle + (float)gyroRate*elapsedTime;
+  gyroAngle = gyroAngle + (float)gyroRate*elapsedTime*0.001;
 
   currentAngle = alpha*(prevAngle + gyroAngle) + (1 - alpha)*accAngle;
-  
-  prevAngle = currentAngle;
-  return currentAngle;
+}
+void calcSpeed() {
+  difference = currentAngle - targetAngle;
+  difSum = difSum + difference;
+  difSum = constrain(difSum, -300, 300);
+//  Serial.println(loopTime);
+  motorSpeed = Kp*difference + Ki*difSum*loopTime - Kd*(currentAngle - prevAngle)/(loopTime*0.001);
 }
