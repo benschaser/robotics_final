9bf55f6a1a96ce8eee7a0ac0c89dbd14c80f0766
Ben Schaser <benschaser@gmail.com>
2022-12-12 00:30:40 -0600

    reversion to stepper motors

--- main/main.ino
+++ main/main.ino
@@ -2,14 +2,22 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_MotorShield.h>
+#include <AccelStepper.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MPU6050 mpu;
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-//Adafruit_StepperMotor *leftMotor = AFMS.getStepper(400, 2);
-//Adafruit_StepperMotor *rightMotor = AFMS.getStepper(400, 1);
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
+Adafruit_StepperMotor *leftMotor = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *rightMotor = AFMS.getStepper(200, 1);
+void forwardstep1() {
+  leftMotor->onestep(BACKWARD, DOUBLE);
+  rightMotor->onestep(FORWARD, DOUBLE);
+}
+void backwardstep1() {
+  leftMotor->onestep(FORWARD, DOUBLE);
+  rightMotor->onestep(BACKWARD, DOUBLE);
+}
+AccelStepper Astepper(forwardstep1, backwardstep1); // use functions to step
 
 // Angle of Inclination
 double accX, accZ;
@@ -19,34 +27,36 @@ double gyroAngle = 0;
 double gyroAngleNext = 0;
 double gyroAnglePrev = 0;
 unsigned long currTime, prevTime = 0;
-double loopTime;
+double loopTime = 0.004;
 double currentAngle = 0;
 double prevAngle = 0;
-double prevPrevAngle = 0;
-double nextAngle = 0;
 double tau = 0.7;
-double dt = 0.005;
-double alpha = tau / (tau + dt);
+//double dt = 0.005;
+double alpha = tau / (tau + loopTime);
 
 // PID Constants
-double Kp = 8.0;
-double Ki = 0.08;
-double Kd = 0.8;
-double speedFactor = 2.0;
+double Kp = 7.5;
+double Ki = 0.00;
+double Kd = 1.0;
 
-double targetAngle = 0;
-double difference = 0;
-double prevDif = 0;
-double difSum = 0;
+double targetAngle = 4.5;
+double minError = -11.5;
+double maxError = 20.5;
+double error = 0;
+double prevError = 0;
+double errSum = 0;
 double motorSpeed = 0;
+double prevMotorSpeed = 0;
+double prevPrevMotorSpeed = 0;
+double motorSpeedAvg = 0;
 double motorSpeedConv = 0;
 double RtoD = 57.2957795131;
-double motorCutoff = 16.0;
+double motorCutoff = 25.0;
 bool active = false;
 int buttonState = 0;
 
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
   mpu.begin();
 
   // set accelerometer range to +-8G
@@ -66,48 +76,51 @@ void setup() {
   //  mpu.setZGyroOffset(-11);
 
   AFMS.begin();
-  leftMotor->setSpeed(0);
-  rightMotor->setSpeed(0);
+  leftMotor->setSpeed(1);
+  rightMotor->setSpeed(1);
   TWBR = ((F_CPU/400000l) - 16) / 2;
 
   pinMode(8, INPUT); // btn
 }
 
 void loop() {
-  buttonState = digitalRead(8);
-//  Serial.println(buttonState);
-  if (buttonState == 1) {
-    active = !active;
-    delay(200);
-  }
-  
-  if (active) {
-    _main();
-  }
-  else {
-    leftMotor->setSpeed(0);
-    rightMotor->setSpeed(0);
-  }
+//  buttonState = digitalRead(8);
+////  Serial.println(buttonState);
+//  if (buttonState == 1) {
+//    active = !active;
+//    delay(200);
+//  }
+//  
+//  if (active) {
+//    _main();
+//  }
+  _main();
 }
 void _main() {
   currTime = millis();
 //  Serial.println(currTime - prevTime);
   loopTime = (currTime - prevTime);
+//  Serial.println(loopTime);
   loopTime = loopTime / 1000;
   prevTime = currTime;
-  dt = loopTime;
+//  dt = loopTime;
+  
   
   getCurrentAngle(loopTime);
-  Serial.print(currentAngle);
-  Serial.print(' ');
+//  Serial.print(currentAngle);
+//  Serial.print(' ');
+//  Serial.print(targetAngle);
+//  Serial.print(' ');
   calcSpeed(currentAngle);
-  motorSpeed = constrain(motorSpeed, -1000, 1000);
-  motorSpeed = map(motorSpeed, -1000, 1000, -255, 255);
-  Serial.println(motorSpeed);
-  motorSpeedConv = map(abs(motorSpeed), 0, 150, 50, 255);
+  motorSpeed = constrain(motorSpeed, -500, 500);
+//  motorSpeed = map(motorSpeed, -1000, 1000, -500, 500);
+//  Serial.println(motorSpeedAvg);
+//  motorSpeedConv = map(abs(motorSpeed), 0, 150, 50, 255);
   drive();
   
   prevAngle = currentAngle;
+//  prevPrevMotorSpeed = prevMotorSpeed;
+  prevMotorSpeed = motorSpeed;
 }
 
 void getCurrentAngle(double elapsedTime) {
@@ -127,37 +140,25 @@ void getCurrentAngle(double elapsedTime) {
 
   gyroRate = map(gyroY, -32768, 32767, -250, 250);
   gyroAngle = gyroAngle + (float)gyroRate * elapsedTime;
-  currentAngle = tau * (prevAngle + gyroAngle*dt) + (1 - tau) * accAngle;
-//  Serial.println(accAngle);
-
-
+  currentAngle = tau * (prevAngle + gyroAngle*loopTime) + (1 - tau) * accAngle;
 }
 void calcSpeed(double angle) {
-  difference = angle - targetAngle;
-  difSum += difference;
-  difSum = constrain(difSum, -300, 300);
-  motorSpeed = Kp * difference + Ki * difSum * loopTime + Kd * (difference - prevDif) / loopTime;
-  prevDif = difference;  
+  error = 4 * constrain(targetAngle - angle, minError, maxError);
+  errSum += error * loopTime;
+  errSum = constrain(errSum, -300, 300);
+  motorSpeed = Kp * error + Ki * errSum + Kd * (error - prevError) / loopTime;
+  prevError = error;  
+//  motorSpeedAvg = 0.2 * prevMotorSpeed + 0.8 * motorSpeed;
+//  Serial.println(motorSpeed);
 }
 void drive() {
-//  if (motorSpeed < -motorCutoff) {
-//    leftMotor->step(1, BACKWARD, DOUBLE);
-//    rightMotor->step(1, FORWARD, DOUBLE);
-//  }
-//  else if (motorSpeed > motorCutoff) {
-//    leftMotor->step(1, FORWARD, DOUBLE);
-//    rightMotor->step(1, BACKWARD, DOUBLE);
-//  }
-//  leftMotor->setSpeed(abs(motorSpeed));
-  leftMotor->setSpeed(motorSpeedConv);
-  rightMotor->setSpeed(motorSpeedConv);
-
-  if (motorSpeed > 0.0) {
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
+  if (motorSpeed > motorCutoff  || motorSpeed < -motorCutoff) {
+    Astepper.setSpeed(-motorSpeed);
+    Astepper.runSpeed();
   }
-  else if (motorSpeed < 0.0) {
-    leftMotor->run(BACKWARD);
-    rightMotor->run(BACKWARD);
+  else {
+    leftMotor->release();
+    rightMotor->release();
   }
+  
 }
--- test/accel-test/accel-test.ino
+++ test/accel-test/accel-test.ino
@@ -21,34 +21,42 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Connect a stepper motor with 200 steps per revolution (1.8 degree)
 // to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myStepper1 = AFMS.getStepper(400, 1);
+Adafruit_StepperMotor *myStepper1 = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *myStepper2 = AFMS.getStepper(200, 1);
 
 // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
 void forwardstep1() {
-  myStepper1->step(1, FORWARD, SINGLE);
+  myStepper1->onestep(FORWARD, DOUBLE);
+  myStepper2->onestep(BACKWARD, DOUBLE);
+
 }
 void backwardstep1() {
-  myStepper1->step(1, BACKWARD, SINGLE);
+  myStepper1->onestep(BACKWARD, DOUBLE);
+  myStepper2->onestep(FORWARD, DOUBLE);
 }
 
 AccelStepper Astepper1(forwardstep1, backwardstep1); // use functions to step
+AccelStepper Astepper2(forwardstep1, backwardstep1); // use functions to step
 
 void setup()
 {
    Serial.begin(9600);           // set up Serial library at 9600 bps
    Serial.println("Stepper test!");
 
-  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-    Serial.println("Could not find Motor Shield. Check wiring.");
-    while (1);
-  }
+  // if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+  //   Serial.println("Could not find Motor Shield. Check wiring.");
+  //   while (1);
+  // }
   Serial.println("Motor Shield found.");
+  AFMS.begin();
 
-  Astepper1.setSpeed(60);
+  Astepper1.setSpeed(-500);
+  Astepper2.setSpeed(-500);
+  TWBR = ((F_CPU/400000l) - 16) / 2;
 }
 
-void loop()
-{
+void loop() {
    Astepper1.runSpeed();
+   delay(0);
 }
